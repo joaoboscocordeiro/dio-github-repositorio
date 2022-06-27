8ad39ba9e3f1a7299ef3bc60bc33bdaff309898c
John Lima <ti.junior@gmail.com>
2022-06-27 11:27:39 -0300

    Conceito de collections/arrays

---
+++ src/main/kotlin/one/digitalinnovation/bank/collections/test/TesteIntArrayOf.kt
@@ -0,0 +1,16 @@
+package src.main.kotlin.one.digitalinnovation.bank.collections.test
+
+fun main() {
+    val values = intArrayOf(2, 3, 4, 1, 10, 7)
+
+    values.forEach {
+        println(it)
+    }
+
+    println("======================================================")
+    values.sort()
+    values.forEach {
+        println(it)
+    }
+
+}
